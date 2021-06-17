6fdab20cd27665f77a2f993cea766c323f21f09c
ephmYZnq <hslibc77@gmail.com>
2021-06-17 15:37:13 +0800

    add yanghui_triangle

--- Java/src/array/pascals_triangle.java
+++ Java/src/array/pascals_triangle.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @version 1.0
  * @date 6/17/21 2:44 PM
  **/
-public class yanghui_triangle {
+public class pascals_triangle {
 
     /*
      * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。 在杨辉三角中，每个数是它左上方和右上方的数的和。
