12cc78e1b6de4fb723fd6470082b79baeef7290d
ephmYZnq <hslibc77@gmail.com>
2021-06-28 13:51:59 +0800

    init search_insert

--- Java/src/array/search_insert.java
+++ Java/src/array/search_insert.java
@@ -42,6 +42,5 @@ public class search_insert {
     public static void main(String[] args) {
         System.out.println(solution(new int[]{1,3,5,6}, 5));
         System.out.println(solution(new int[]{1,3,5,6}, 2));
-        System.out.println(solution(new int[]{1,3,5,6}, 7));
     }
 }
